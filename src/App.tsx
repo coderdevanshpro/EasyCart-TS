484ece377d3986eada415a55de5390a3f933e0fa
coderdevanshpro <devanshsharma75cp@gmail.com>
2024-09-15 23:18:26 +0530

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { MouseEvent } from 'react';
 import ProductDetail from './ProductDetail.tsx'
 import Button from './Button.tsx';
+import Greeting from './Greeting.tsx';
+import Greeting2 from './Greeting2.tsx';
 
 
 function App() {
-  function handleClick(event:MouseEvent<HTMLButtonElement>)
+  const [count,setCount] = useState<number>();
+  function Increment(event:MouseEvent<HTMLButtonElement>)
   {
     console.log(event.target);
   }
@@ -17,12 +20,14 @@ function App() {
 
   return (
     <>
-    <Button>SignUp</Button>
-    <button onClick={handleClick}></button>
+    {/* <Button>SignUp</Button>
+    <button onClick={Increment}></button>
     <input onChange={handleChange}></input>
-      <ProductDetail title="iPhone" description="hello world" price={99}></ProductDetail>
+      <ProductDetail title="iPhone" description="hello world" price={99}></ProductDetail> */}
+      <Greeting name="Dumbledore"></Greeting>
+      <Greeting2 name="Harry Puttar"></Greeting2>
     </>
   )
 }
 
-export default App
+export default App;
